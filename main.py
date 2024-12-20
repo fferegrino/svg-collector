b7c492b4396770bc21c2904eae3da0f2579d42f7
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-20 16:38:10 +0000

    Accept size specification

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ files.mkdir(parents=True, exist_ok=True)
 
 user_input = input("Enter the URL: ")
 
-desired_widths = [500, 1000, 2000]
+desired_sizes = [500, 1000, 2000]
 
 while user_input != "q":
     
@@ -21,14 +21,20 @@ while user_input != "q":
     with open(files / path.name, "wb") as f:
         f.write(response.content)
 
+    argument_to_modify = "output_width"
     if arguments:
-        desired_widths = [int(arguments[0])]
+        desired_sizes = [int(arguments[0])]
 
-    for width in desired_widths:
+        if len(arguments) > 1:
+            specification = arguments[1]
+            if specification == "-h":
+                argument_to_modify = "output_height"
+
+    for size in desired_sizes:
         cairosvg.svg2png(
             bytestring=response.content,
-            write_to=f"{files / path.name}_{width}.png",
-            output_width=width
+            write_to=f"{files / path.name}_{size}.png",
+            **{argument_to_modify: size}
         )
 
     user_input = input("Enter the URL: ")
